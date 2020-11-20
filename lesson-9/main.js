55f9144b62db60ee0607d28d179320c9d0fe5aae
Ivan Stupchik <hardtrynew@rambler.ru>
2020-11-20 20:22:21 +0300

    homework-9

---
+++ lesson-9/main.js
@@ -0,0 +1,58 @@
+function Animal(name) {
+    var self = this;
+
+    self.name = name;
+    self.foodAmount = 50;
+
+    function formatFoodAmount() {
+        return self.foodAmount + ' гр.';
+    }
+
+    self.dailyNorm = function(amount) {
+        if (!arguments.length) return formatFoodAmount();
+
+        if (amount < 50 || amount > 500) {
+            return 'Недопустимое количество корма.';
+        }
+
+        self.foodAmount = amount;
+    };
+
+    self.feed = function() {
+        console.log('Насыпаем в миску ' + self.dailyNorm() + ' корма.');
+        
+        return self;
+    };
+}
+
+function Cat() {
+    Animal.apply(this, arguments);
+
+    var animalFeed = this.feed;  
+    this.feed = function() {
+        animalFeed();
+        this.happyCat();
+    };
+    
+    this.happyCat = function() {
+      console.log('Кот доволен ^_^');
+    };
+
+    this.stroke = function() {
+        console.log('Гладим кота');
+
+        return this;
+    };
+}
+
+
+var barsik = new Cat('Барсик');
+
+console.log(barsik.name);
+
+console.log(barsik.dailyNorm());
+console.log(barsik.feed());
+console.log(barsik.stroke().feed());
+
+
+barsik = null;
