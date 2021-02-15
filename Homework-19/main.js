1e2049fecd81b00ba59fcb243640662a16584848
IvanStupchuk <hardtrynew@rambler.ru>
2021-02-15 12:39:41 +0300

    Homework-19 fix

--- Homework-19/main.js
+++ Homework-19/main.js
@@ -1,17 +1,80 @@
-var mainBtn = document.getElementById('main-btn'),
+const mainBtn = document.getElementById('main-btn'),
     btnsSettings = document.getElementsByClassName('btns-setting')[0],
     mnt = document.getElementsByClassName('minutes')[0],
     scd = document.getElementsByClassName('seconds')[0],
     ms = document.getElementsByClassName('milliseconds')[0],
     stopwatch = document.getElementsByClassName('watch')[0],
-    timer;
+    body = document.getElementsByTagName('body')[0],
+    result = document.getElementsByClassName('result')[0],
+    resetBtn = document.getElementById('btn-reset'),
+    saveBtn = document.getElementById('btn-save');
 
-var msStart = 0,
+
+let timer;
+
+let msStart = 0,
     scdStart = 0,
     mntStart = 0;
 
-function watch() {
+body.onunload = () => {
+    localStorage.setItem('msStart', ms.innerHTML); 
+    localStorage.setItem('scdStart', scd.innerHTML); 
+    localStorage.setItem('mntStart', mnt.innerHTML); 
+
+    localStorage.setItem('dataState', stopwatch.dataset.state);
+    localStorage.setItem('result', result.innerHTML);
+};
+
+body.onload = () => {
+    if (!localStorage.getItem('msStart')) {
+        ms.innerHTML = '00';
+        scd.innerHTML = '00';
+        mnt.innerHTML = '00';
+    }
+    else {
+        ms.innerHTML = localStorage.getItem('msStart');
+        scd.innerHTML = localStorage.getItem('scdStart');
+        mnt.innerHTML = localStorage.getItem('mntStart');
+    }
+
+    if (!localStorage.getItem('dataState')) {
+        stopwatch.dataset.state = 'initial';
+    }
+    else {
+        stopwatch.dataset.state = localStorage.getItem('dataState');
+    }
+
+    msStart = +localStorage.getItem('msStart');
+    scdStart = +localStorage.getItem('scdStart');
+    mntStart = +localStorage.getItem('mntStart');
+
+    switch(stopwatch.dataset.state) {
+        case 'running':
+            timer = setInterval(watch, 10);
+            mainBtn.innerHTML = 'Stop';
+            btnsSettings.classList.add('btns-setting-show');
+            break;
+
+        case 'stopped':        
+            clearInterval(timer);
+            mainBtn.innerHTML = 'Run';
+            btnsSettings.classList.add('btns-setting-show');
+            break;
+    }
+    result.innerHTML = localStorage.getItem('result');
+};
+
+const appearance = (displayArea, value) => {
+    displayArea.innerHTML ='0' + value;
+
+    if (value > 9) {
+        displayArea.innerHTML = value;
+    }
+}
+
+const watch = () => {
     msStart++;
+
     if (msStart === 100) {
         msStart = 0;
         scdStart++;
@@ -24,26 +87,17 @@ function watch() {
 
     if (mntStart === 60) {
         clearInterval(timer);
-        saveBtn.classList.add('btns-setting');
-    }
-    
-    ms.innerHTML ='0' + msStart;
-    if (msStart > 9) {
-        ms.innerHTML = msStart;
+        saveBtn.remove();
+        mainBtn.remove();
+        msStart = 0;
     }
 
-    scd.innerHTML ='0' + scdStart;
-    if (scdStart > 9) {
-        scd.innerHTML = scdStart;
-    }
-
-    mnt.innerHTML ='0' + mntStart;
-    if (mntStart > 9) {
-        mnt.innerHTML = mntStart;
-    }
+    appearance(ms, msStart);
+    appearance(scd, scdStart);
+    appearance(mnt, mntStart);
 }
 
-mainBtn.addEventListener('click', function() {
+mainBtn.addEventListener('click', () => {
     btnsSettings.classList.add('btns-setting-show');
 
     switch(stopwatch.dataset.state) {
@@ -67,9 +121,7 @@ mainBtn.addEventListener('click', function() {
     }
 });
 
-var resetBtn = document.getElementById('btn-reset');
-
-resetBtn.addEventListener('click', function() {
+resetBtn.addEventListener('click', () => {
     btnsSettings.classList.remove('btns-setting-show');
     clearInterval(timer);
     ms.innerHTML ='00';
@@ -83,15 +135,14 @@ resetBtn.addEventListener('click', function() {
     stopwatch.dataset.state = 'initial';
 });
 
-var saveBtn = document.getElementById('btn-save'),
-    result = document.getElementsByClassName('result')[0];
-
-saveBtn.addEventListener('click', function() {
-    var a = result.children,
+saveBtn.addEventListener('click', () => {
+    let a = result.children,
         saveNumber = a.length + 1,
         newParagraph = document.createElement('p');
 
-    newParagraph.innerHTML = saveNumber + ') ' + mnt.innerHTML + ' : ' + scd.innerHTML + ' : ' + ms.innerHTML;
+    newParagraph.innerHTML = `${saveNumber}) ${mnt.innerHTML} : ${scd.innerHTML} : ${ms.innerHTML}`;
     result.appendChild(newParagraph);
 });
+
+
 
